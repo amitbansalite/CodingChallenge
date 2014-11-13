57baad7ba400a39b5251159b0cc70432925467fd
unknown <agarwala@RST-DEVAMITA.Riversand.com>
2014-11-13 10:29:28 +0530

    Commiting the entire console application which is used to analyse a log file

---
+++ CodingChallenge/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace CodingChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Validations
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Invalid command line arguments. Please enter one one argument which is the path to the log file.");
+                Console.WriteLine("Press Enter to exit.");
+                Console.ReadLine();
+            }
+
+            var fileName = args[0];
+            if (File.Exists(fileName) == false)
+            {
+                Console.WriteLine("File cannot be found at the specified location. Please enter a valid path to the log file");
+                Console.WriteLine("Press Enter to exit.");
+                Console.ReadLine();
+            }
+
+            // initial setup
+            var responses = new List<Response>();
+            responses.Add(new Response("GET /api/users/{user_id}", @"GET.*users\/\d+ "));
+            responses.Add(new Response("POST /api/users/{user_id}", @"POST.*users\/\d+ "));
+            responses.Add(new Response("count_pending_messages"));
+            responses.Add(new Response("get_messages"));
+            responses.Add(new Response("get_friends_progress"));
+            responses.Add(new Response("get_friends_score"));
+
+            // parsing the file, line by line and saving the analysis
+            var lineNumber = 0;
+            using (StreamReader reader = File.OpenText(fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    lineNumber++;
+                    for (int x = 0; x < responses.Count; x++)
+                    {
+                        var found = responses[x].AnalyseLog(line, lineNumber);
+                        if (found)
+                            break;
+                    }
+                }
+            }
+
+            foreach (var result in responses)
+            {
+                Console.WriteLine(result.ToString());
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CodingChallenge/Response.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace CodingChallenge
+{
+    public class Response
+    {
+        private String Url;
+        private Regex regex;
+        private int countCalls;
+        private int totalSumResponseTime;
+        private SortedDictionary<int, int> modes;
+        private Dictionary<String, int> dynos = new Dictionary<string, int>();
+
+        public Response(String url)
+        {
+            Url = url;
+            modes = new SortedDictionary<int, int>();
+            dynos = new Dictionary<string, int>();
+        }
+
+        public Response(String url, String regexPattern)
+            : this(url)
+        {
+            regex = new Regex(regexPattern);
+        }
+
+        public bool AnalyseLog(String text, int lineNumber)
+        {
+            try
+            {
+                if ((regex != null && regex.Match(text).Success) || text.Contains(Url))
+                {
+                    countCalls++;
+                    var content = text.Split(' ');
+
+                    var connect = content[8].Split('=')[1];
+                    var connectTime = connect.Remove(connect.Length - 2);
+                    var service = content[9].Split('=')[1];
+                    var serviceTime = service.Remove(service.Length - 2);
+
+                    var responseTime = Convert.ToInt32(connectTime) + Convert.ToInt32(serviceTime);
+                    totalSumResponseTime += responseTime;
+
+                    int currentModeCount;
+                    modes.TryGetValue(responseTime, out currentModeCount);
+                    modes[responseTime] = currentModeCount + 1;
+
+                    int currentDynoCount;
+                    var currentDyno = content[7].Split('=')[1];
+                    dynos.TryGetValue(currentDyno, out currentDynoCount);
+                    dynos[currentDyno] = currentDynoCount + 1;
+
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error encountered while parsing the log file. The format at line number {0} is incorrect : ", lineNumber);
+            }
+            return false;
+        }
+
+        public string GetMostFrequentDyno()
+        {
+            if (dynos.Any())
+            {
+                KeyValuePair<string, int> mostOccuring = dynos.First();
+                foreach (KeyValuePair<string, int> dyno in dynos)
+                {
+                    if (dyno.Value > mostOccuring.Value)
+                        mostOccuring = dyno;
+                }
+                return mostOccuring.Key;
+            }
+            return "None";
+        }
+
+        public int GetMode()
+        {
+            if (modes.Any())
+                return modes.Keys.Last();
+            else
+                return 0;
+        }
+
+        public int GetMean()
+        {
+            if (countCalls > 0)
+                return totalSumResponseTime / countCalls;
+            else
+                return 0;
+        }
+
+        // The SortedDictionary stores the frequency distribution. 
+        // To find the median, the group(key) which contains the median is being searched and returned
+        public int GetMedian()
+        {
+            if (modes.Any())
+            {
+                var position = modes.Count/2;
+                var elements = 0;
+                foreach (KeyValuePair<int, int> elem in modes)
+                {
+                    elements += elem.Value;
+                    if (elements >= position)
+                        return elem.Key;                    
+                }                
+            }
+
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            var str = new StringBuilder();
+            str.Append(String.Format("\n Url = {0} ", Url));
+            str.Append(String.Format("\n Number of times accessed     = {0}", countCalls));
+            str.Append(String.Format("\n Mean   response time         = {0}ms ", GetMean()));
+            str.Append(String.Format("\n Median response time         = {0}ms ", GetMedian()));
+            str.Append(String.Format("\n Mode   response time         = {0}ms ", GetMode()));
+            str.Append(String.Format("\n dyno that responded the most = {0} ", GetMostFrequentDyno()));
+            return str.ToString();
+        }
+    }
+}
